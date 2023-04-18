71f6de8f42f1c4bcf8ed712837bd8567bc605036
Your Name <christianmendez@Christians-MBP.attlocal.net>
2023-04-17 22:19:43 -0500

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-console.log(process.env.MONGODB_URI, "christian christian");
-console.log(process.env.TORRE);
+// console.log(process.env.MONGODB_URI, "christian christian");
+// console.log(process.env.TORRE);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
