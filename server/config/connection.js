991325eaaf3858569f7e15db4663c228dbe65ad9
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2023-04-17 21:20:07 -0500

    does mongodb works

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+console.log(process.env.MONGODB_UR, "kdaksljasdlkjfakslddsjkfasdljkadsfjaklsdasdf");
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
